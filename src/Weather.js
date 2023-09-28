f5488971c2f285a59268748e5efff046c63bce3d
Nattanon Siripaisananusorn <139682335+Nattan0n@users.noreply.github.com>
2023-09-28 20:19:54 +0700

    Add files via upload

--- src/Weather.js
+++ src/Weather.js
@@ -1,43 +1,63 @@
-import React, { useState } from 'react'
-import { View, Text, StyleSheet, TextInput, Button } from "react-native";
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, TextInput, Button, Alert } from "react-native";
 import axios from 'axios';
 
 const Weather = () => {
     const [city, setCity] = useState("");
     const [weather, setWeather] = useState(null);
-    //https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
     const API_KEY = "9cbf6226a3b25f210b469efd448e311b";
     const BASE_URL = "https://api.openweathermap.org/data/2.5/weather";
+    const [errorText, setErrorText] = useState("");
 
     const getWeather = async () => {
         try {
+            const trimmed_City = city.trim();
             const response = await axios.get(
-                `${BASE_URL}?q=${city}&appid=${API_KEY}`
+                `${BASE_URL}?q=${trimmed_City}&appid=${API_KEY}`
             );
             console.log(response.data);
-            setWeather(response.data);
-        }catch (error) {
-            console.error("Error fetching weather data",error);
+            const responseData = response.data;
+    
+            if (!responseData.name) {
+                setErrorText("City not found");
+                setWeather(null);
+                Alert.alert("City not found", "ไม่พบเมืองที่คุณป้อน.");
+            } else {
+                setErrorText("");
+                setWeather(responseData);
+            }
+        } catch (error) {
+            console.error("Error fetching weather data", error);
+            setErrorText("Error fetching weather data");
+            Alert.alert("Error", "เกิดข้อผิดพลาดขณะดึงข้อมูลสภาพอากาศ กรุณาตรวจสอบว่าคุณป้อนชื่อเมืองหรือประเทศให้ถูกต้อง");
         }
     };
+    
 
-    return(
+    const clearData = () => {
+        setCity("");
+        setWeather(null);
+    };
+
+    return (
         <View>
             <Text style={styles.text}>Weather🌍</Text>
-            <TextInput 
+            {/* <Text style={styles.errorText}>{errorText}</Text> */}
+            <TextInput
                 value={city}
-                onChangeText={(newText)=>setCity(newText)}
-                placeholder=''
+                onChangeText={(newText) => setCity(newText)}
+                placeholder=""
                 style={styles.container}
             />
-            <Button title='Get weather' onPress={getWeather}/>
+            <Button title="Get weather" onPress={getWeather} />
             {weather && (
-                <View style={{marginBottom:10 ,marginTop:50 , backgroundColor:'#fff',}}>
+                <View style={{ marginBottom: 10, marginTop: 50, backgroundColor: '#fff' }}>
                     <Text style={styles.text}>
                         Temperature 🌡️: {Math.round(weather.main.temp - 273.15)} ํC
                     </Text>
                     <Text style={styles.text}>Humidity 💧: {weather.main.humidity}</Text>
                     <Text style={styles.text}>Condition: {weather.weather[0].description}</Text>
+                    <Button title="Clear All" color={'red'} onPress={clearData} />
                 </View>
             )}
         </View>
@@ -45,21 +65,28 @@ const Weather = () => {
 }
 
 const styles = StyleSheet.create({
-    text:{
-        textAlign:'center',
-        margin:10,
-        fontSize:20,
+    text: {
+        textAlign: 'center',
+        margin: 10,
+        fontSize: 20,
     },
-    container:{
-        width:300,
-        borderRadius:5,
-        borderWidth:2 ,
-        borderColor:"#000",
-        backgroundColor:'#fff',
-        padding:10,
-        marginBottom:10,
+    container: {
+        width: 300,
+        borderRadius: 5,
+        borderWidth: 2,
+        borderColor: "#000",
+        backgroundColor: '#fff',
+        padding: 10,
+        marginBottom: 10,
         textAlign: 'center',
     },
+    // errorText: {
+    //     textAlign: 'center',
+    //     margin: 10,
+    //     fontSize: 20,
+    //     color: 'red',
+    // },
 });
 
 export default Weather;
+
